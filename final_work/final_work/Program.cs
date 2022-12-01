3886fac967b0ac8fce764bd28a91e1e9f9ee082c
Богдан К <epam.2@mail.ru>
2022-12-01 16:24:13 +0400

    create CreateArray() method

--- final_work/final_work/Program.cs
+++ final_work/final_work/Program.cs
@@ -8,13 +8,7 @@ namespace final_work
         static void Main(string[] args)
         {
             
-            string[] array = CreateArray();
-            ShowArray(array);
-
-            string[] shortArray = ShortStrings(array);
-            ShowArray(shortArray);
-
-            Console.ReadKey();
+            
         }
 
         static string[] CreateArray()
@@ -22,35 +16,6 @@ namespace final_work
             string[] arr = new string[] { "integer", ":-)!!!", "Try", "IF" };
             return arr;
 
-        }
-
-        static string[] ShortStrings(string[] arr)
-        {
-            for (int i = 0; i < arr.Length; i++)
-            {
-                if (arr[i].Length > 3)
-                {
-                    arr[i] = arr[i].Remove(3);
-                }
-
-            }
-
-            return arr;
-        }
-
-        static void ShowArray(string[] arr)
-        {
-            Console.WriteLine(String.Empty);
-            for (int i = 0; ; i++)
-            {
-                if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
-                else if (i == arr.Length - 1)
-                {
-                    Console.Write(arr[i] + ".");
-                    Console.WriteLine(String.Empty);
-                    return;
-                }
-            }
         }
 
 
